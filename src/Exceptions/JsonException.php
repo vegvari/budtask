eecfa1f9d2b226f362cea770e7f0956addc3f841
Romeo Vegvari <vegvari@users.noreply.github.com>
2019-05-13 19:39:00 +0100

    Unused param removed

--- src/Exceptions/JsonException.php
+++ src/Exceptions/JsonException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class JsonException extends Exception
 {
-	public function __construct(string $message)
+	public function __construct()
 	{
 		parent::__construct(sprintf('Json decode error: "%s"', json_last_error_msg()));
 	}
